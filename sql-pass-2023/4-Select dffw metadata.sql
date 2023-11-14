a068be46cbf3a72b6f1b4a8d8ec8563452706338
aleonard763 <andy.leonard@gmail.com>
2023-11-14 18:50:36 -0500

    Add files via upload

---
+++ sql-pass-2023/4-Select dffw metadata.sql
@@ -0,0 +1,11 @@
+Select * From dffw.[application]
+
+Select * From dffw.pipeline
+
+Select * From dffw.applicationpipeline
+
+Select * From dffw.applicationpipelineparameter
+
+exec dffw.getApplicationPipelines @ApplicationName = N'Application Five'
+
+exec dffw.getApplicationPipelineParameters @ApplicationName = N'Application Five', @PipelineName = N'v5-pipeline1'
