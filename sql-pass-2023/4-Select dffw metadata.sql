619c330945687f4994d40c4678ca491f8eb0eba6
aleonard763 <andy.leonard@gmail.com>
2025-01-06 11:45:57 -0500

    Update 4-Select dffw metadata.sql

--- sql-pass-2023/4-Select dffw metadata.sql
+++ sql-pass-2023/4-Select dffw metadata.sql
@@ -9,3 +9,5 @@ Select * From dffw.applicationpipelineparameter
 exec dffw.getApplicationPipelines @ApplicationName = N'Application Five'
 
 exec dffw.getApplicationPipelineParameters @ApplicationName = N'Application Five', @PipelineName = N'v5-pipeline1'
+
+ -- (c) Copyright 2021, Enterprise Data & Analytics
